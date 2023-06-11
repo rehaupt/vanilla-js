07926ff8ee3d53d3d1d9dc2246d190fede73d51f
Ronald Haupt <ronh@ronh-mbp-2.local>
2023-06-11 15:59:30 -0700

    Initial basic JS with webpack and babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import '../styles/style.css'
+// import javascriptLogo from '../javascript.svg'
+console.log('Hello there Vanilla JS Project')
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <h1>Hello Renton!</h1>
+//     <div>
+// <h4>my card </h4>
+//     </div>
+
+//   </div>
